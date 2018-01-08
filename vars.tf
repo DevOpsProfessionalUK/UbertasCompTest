a8cd0bc5e199b1adacfb030bace628bf19c53dbe
root <root@ansnode2.(none)>
2018-01-08 14:58:45 -0800

    adding the right AMI Image ID for Ubuntu

--- vars.tf
+++ vars.tf
@@ -6,9 +6,7 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = "map"
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
+    eu-west-1 = "ami-8fd760f6"
   }
 }
 
